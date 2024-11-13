8b31a0c689dcd6cd13f5093441615439a92a66ba
sen3838 <tpsk9972@naver.com>
2024-11-13 09:07:42 +0900

    title 포트폴리오

---
+++ js/index.js
@@ -0,0 +1,18 @@
+$(function () {
+  //   let baseline = -300;
+  let visual = $("#visual").height();
+  let profile = $("#con1").offset().top - 500;
+  let key = $("#con2 .wrap").offset().top - 1000;
+  console.log(visual);
+  $(window).on("scroll", function () {
+    let sc = $(window).scrollTop();
+    // console.log(sc);
+    if ((visual <= sc) & (sc >= profile)) {
+      $("#con1 .id_pic li:nth-child(1)").addClass("on");
+      $("#con1 .id_pic li:nth-child(3)").addClass("on");
+    } else if (sc >= key) {
+      $("#con2 .left").addClass("on");
+      $("#con2 .right ul").addClass("on");
+    }
+  });
+});

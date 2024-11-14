f985fd14ab5d856260242e8d431fc519f3a82eeb
sen3838 <tpsk9972@naver.com>
2024-11-15 08:41:33 +0900

    porfolio

--- js/index.js
+++ js/index.js
@@ -1,18 +1,18 @@
-$(function () {
-  //   let baseline = -300;
-  let visual = $("#visual").height();
-  let profile = $("#con1").offset().top - 500;
-  let key = $("#con2 .wrap").offset().top - 1000;
-  console.log(visual);
-  $(window).on("scroll", function () {
-    let sc = $(window).scrollTop();
-    // console.log(sc);
-    if ((visual <= sc) & (sc >= profile)) {
-      $("#con1 .id_pic li:nth-child(1) p").addClass("on");
-      $("#con1 .id_pic li:nth-child(3) p").addClass("on");
-    } else if (sc >= key) {
-      $("#con2 .left").addClass("on");
-      $("#con2 .right ul").addClass("on");
-    }
-  });
-});
+// $(function () {
+//   //   let baseline = -300;
+//   let visual = $("#visual").height();
+//   let profile = $("#con1").offset().top - 500;
+//   let key = $("#con2 .wrap").offset().top - 1000;
+//   console.log(visual);
+//   $(window).on("scroll", function () {
+//     let sc = $(window).scrollTop();
+//     // console.log(sc);
+//     if ((visual <= sc) & (sc >= profile)) {
+//       $("#con1 .id_pic li:nth-child(1) p").addClass("on");
+//       $("#con1 .id_pic li:nth-child(3) p").addClass("on");
+//     } else if (sc >= key) {
+//       $("#con2 .left").addClass("on");
+//       $("#con2 .right ul").addClass("on");
+//     }
+//   });
+// });
